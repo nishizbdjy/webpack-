c7130bc191ece551e6fbaa5433b6ce56c584eb50
lbw <2659154921@qq.com>
2019-12-24 16:52:05 +0800

    完成css、less的打包

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+//引入样式
+import './index.less';
+import './style.css'
 let box = document.createElement('div')
 box.innerHTML = ` 
-<p>这是node代码 no</p>
+<div class="box">这是node代码 no</div>
 `
 document.body.appendChild(box)//添加到 页面
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path =require('path')//引入
+const path = require('path')//引入
 //开始打包
 
 module.exports = {//暴露
@@ -6,7 +6,26 @@ module.exports = {//暴露
     output: {
         filename: './bundle.js',//默认打包后的文件名(默认就是这个)
         path: path.resolve(__dirname, 'dist')//打包后的文件目录(默认也是这个) 
+    },
+    // 模块加载器配置项
+    module: {
+        rules: [
+            {
+                test: /\.css$/,			// 匹配css扩展名文件
+                use: [					// 配置loader加载器
+                    'style-loader',		// 把css代码写入到网页中
+                    'css-loader'		// 读取css的代码
+                ]
+            },
+            {
+                test : /.less$/,  //匹配less
+                use : [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            }
+        ]
     }
-
 
 }
